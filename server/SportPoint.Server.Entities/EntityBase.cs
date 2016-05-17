0ded29cde38c6dfbb60d3df8720b871be326358f
thorisangelo@hotmail.com <thorisangelo@hotmail.com>
2016-05-17 14:09:35 -0300

    Adicionando comentários à classe base das entidades.

--- server/SportPoint.Server.Entities/EntityBase.cs
+++ server/SportPoint.Server.Entities/EntityBase.cs
@@ -6,14 +6,32 @@ using System.Threading.Tasks;
 
 namespace SportPoint.Server.Entities
 {
+    /// <summary>
+    /// Classe base da entidade que possui informações básicas do registro.
+    /// </summary>
     public class EntityBase
     {
         #region Properties
 
+        /// <summary>
+        /// Propriedade que configura/retorna o login do usuário que criou o registro.
+        /// </summary>
         public string CriadoPor { get; set; }
+        /// <summary>
+        /// Propriedade que configura/retorna a data de criação do registro.
+        /// </summary>
         public DateTime DataCriacao { get; set; }
+        /// <summary>
+        /// Propriedade que configura/retorna o login do usuário que modificou o registro.
+        /// </summary>
         public string ModificadoPor { get; set; }
+        /// <summary>
+        /// Propriedade que configura/retorna a data de modificação do registro.
+        /// </summary>
         public DateTime DataModificacao { get; set; }
+        /// <summary>
+        /// Propriedade que configura/retorna um flag se o registro está ativo.
+        /// </summary>
         public bool IsAtivo { get; set; }
 
         #endregion
