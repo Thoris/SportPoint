6cdd8239b8869fd429c8e36547837a0ad841b839
thorisangelo@hotmail.com <thorisangelo@hotmail.com>
2016-05-17 13:52:33 -0300

    Adicionando base da entidade.

---
+++ server/SportPoint.Server.Entities/EntityBase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportPoint.Server.Entities
+{
+    public class EntityBase
+    {
+        #region Properties
+
+        public string CriadoPor { get; set; }
+        public DateTime DataCriacao { get; set; }
+        public string ModificadoPor { get; set; }
+        public DateTime DataModificacao { get; set; }
+        public bool IsAtivo { get; set; }
+
+        #endregion
+    }
+}
